3efa1f1c568104d6a9ee24ec47be0c9065f6d948
jarl1409 <jhosepandrey203@gmail.com>
2025-02-18 21:49:27 -0500

    agregando carrito dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header cart={cart} />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
